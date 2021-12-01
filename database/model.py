04da48b9ba235bb1e49497dc9ffaac8e0a968c57
Alaa Aqeel <alaa.21.iraq@gmail.com>
2021-12-01 12:03:08 -0500

    updaet database/model.py

--- database/model.py
+++ database/model.py
@@ -5,30 +5,32 @@ from sqlalchemy import Column
 from database import SessionLocal, engine
 
 class CustomProperty(property):
-    
+    """Custom Property To call func"""
+
     def __get__(self, _, cls):
         return self.fget(cls)
 
 # Base Model
 class BaseModel(object):
-    
-    session = SessionLocal()
+    """Base Model for  SqlAlchemy Model"""
 
-    id = Column(types.Integer, primary_key=True, index=True)
+    session:object = SessionLocal()
+
+    id:Column = Column(types.Integer, primary_key=True, index=True)
 
     @declared_attr
-    def __tablename__(cls):
+    def __tablename__(cls) -> str:
+        """Set a class name for the table """
         return cls.__name__.lower()
 
     @CustomProperty  
-    def query(cls):
+    def query(cls) -> object:
+        """Get session query """
         return cls.session.query(cls)
 
-
-
     @classmethod 
-    def create(cls, **kw):
-        """Create new object"""
+    def create(cls, **kw:dict) -> list(bool, object):
+        """Create new raw"""
         try: 
             obj = cls(**kw)
             cls.session.add(obj)
@@ -37,15 +39,23 @@ class BaseModel(object):
         except exc.SQLAlchemyError as err:
             return False, str(err.__dict__.get("orig", ""))
 
-    def save(self):
-        self.session.add(self)
-        self.session.commit()
+    def save(self) -> list(bool, object):
+        """Commit object"""
+        try: 
+            self.session.add(self)
+            self.session.commit()
+            self.session.refresh(self)
+            return True, self
+        except exc.SQLAlchemyError as err:
+            return False, str(err.__dict__.get("orig", ""))
         
     def __repr__(self) -> str:
-        return f"<{self.__class__.__name__} id={self.id}/>"
+
+        return f"<{self.__class__.__name__}(id={self.id})>"
     
 
 Model = declarative_base(cls=BaseModel)
 
 def migrate():
+    
     Model.metadata.create_all(bind=engine)
